cbd041afc2c677f4fad3cd80548aae3eef07def5
NicoLaval <nicolas.laval@insee.fr>
2021-01-15 10:12:10 +0100

    Exclude spark local in production mode

--- src/components/common/menu/component.js
+++ src/components/common/menu/component.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Menu as WilcoMenu } from '@inseefr/wilco';
+import { getEnv } from 'utils/env';
+import { IN_MEMORY, SPARK_CLUSTER, SPARK_LOCAL } from 'utils/constants';
 
 const paths = [
-	{ label: 'In Memory', path: '/in-memory' },
-	{ label: 'Spark - Local', path: '/spark' },
-	{ label: 'Spark - Cluster', path: '/spark-cluster' },
+	{ label: 'In Memory', path: IN_MEMORY },
+	{ label: 'Spark - Local', path: SPARK_LOCAL },
+	{ label: 'Spark - Cluster', path: SPARK_CLUSTER },
 	{ label: 'In Js', path: '/in-js', alignToRight: true },
 ];
 
 const Menu = () => {
 	const { pathname } = useLocation();
-	const realPaths = paths.map((p) =>
-		pathname === p.path || pathname.startsWith(`${p.path}/`)
-			? { ...p, className: 'active' }
-			: p
-	);
+	const realPaths = paths
+		.map((p) =>
+			pathname === p.path || pathname.startsWith(`${p.path}/`)
+				? { ...p, className: 'active' }
+				: p
+		)
+		.filter(({ path }) => path !== SPARK_LOCAL || getEnv() !== 'production');
 	return <WilcoMenu paths={realPaths} />;
 };
 
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,2 +1,4 @@
 export const getEnvVar = (key) =>
 	window?._env_?.[key] || process.env[`REACT_APP_${key}`];
+
+export const getEnv = () => process.env.NODE_ENV;

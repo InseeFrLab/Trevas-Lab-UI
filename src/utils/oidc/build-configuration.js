414d6e6d5fbe44d2434f91eb17d203b5df41f7ad
NicoLaval <nicolas.laval@insee.fr>
2021-01-15 16:41:59 +0100

    Fix OIDC configuration

--- src/utils/oidc/build-configuration.js
+++ src/utils/oidc/build-configuration.js
@@ -1,20 +1,21 @@
+import { getEnvVar } from 'utils/env';
+
 export const buildOidcConfiguration = (baseConfig) => {
-  const {
-    REACT_APP_OIDC_AUTHORITY: authority,
-    REACT_APP_OIDC_CLIENT_ID: client_id,
-  } = process.env;
-  const { origin } = window.location;
-  const updatedBaseConfigConfig = Object.entries(baseConfig.config).reduce(
-    (acc, [k, v]) => ({
-      ...acc,
-      [k]: typeof v === "string" ? v.replace("my_origin", origin) : v,
-    }),
-    {}
-  );
-  const configuration = {
-    ...baseConfig,
-    origin: window.location.origin,
-    config: { ...updatedBaseConfigConfig, authority, client_id },
-  };
-  return configuration;
+	const authority = getEnvVar('OIDC_AUTHORITY');
+	const client_id = getEnvVar('OIDC_CLIENT_ID');
+
+	const { origin } = window.location;
+	const updatedBaseConfigConfig = Object.entries(baseConfig.config).reduce(
+		(acc, [k, v]) => ({
+			...acc,
+			[k]: typeof v === 'string' ? v.replace('my_origin', origin) : v,
+		}),
+		{}
+	);
+	const configuration = {
+		...baseConfig,
+		origin: window.location.origin,
+		config: { ...updatedBaseConfigConfig, authority, client_id },
+	};
+	return configuration;
 };

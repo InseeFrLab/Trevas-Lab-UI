ddd0259cecdf5bbf66f1d3f9edf10cc8b988a5a9
NicoLaval <nicolas.laval@insee.fr>
2021-05-11 08:36:32 +0200

    Remove unused

--- src/components/editor-temp/grammar/vtl-2.0-insee-old/VtlVisitor.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Generated from ../../vtl-sources/Vtl-2.0/Vtl.g4 by ANTLR 4.9.0-SNAPSHOT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=VtlVisitor.js.map

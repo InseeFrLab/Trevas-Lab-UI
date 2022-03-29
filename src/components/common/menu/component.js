a8be18ab783b6d63e5a23c2a3434792ac82466bc
NicoLaval <nicolas.laval@insee.fr>
2022-03-29 08:47:48 +0200

    Update menu

--- src/components/common/menu/component.js
+++ src/components/common/menu/component.js
@@ -2,28 +2,21 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Menu as WilcoMenu } from '@inseefr/wilco';
 import { getEnv } from 'env';
-import {
-	IN_MEMORY,
-	SPARK_LOCAL,
-	SPARK_STATIC,
-	SPARK_KUBE,
-	V2_MEMORY,
-	V2_CLUSTER_KUBERNETES,
-} from 'utils/constants';
+import { V2_MEMORY, V2_CLUSTER_KUBERNETES } from 'utils/constants';
 import './menu.scss';
 
 const paths = [
-	{ label: 'In Memory', path: `/${IN_MEMORY}` },
-	{ label: 'Spark - Local', path: `/${SPARK_LOCAL}` },
-	{ label: 'Spark - Static', path: `/${SPARK_STATIC}` },
-	{ label: 'Spark - Kube', path: `/${SPARK_KUBE}` },
-	{ label: 'Build Parquet', path: '/build-parquet', alignToRight: true },
-	{ label: 'In Js', path: '/in-js', alignToRight: true },
-	{ label: 'V2 - In Memory', path: `/${V2_MEMORY}`, alignToRight: true },
+	// { label: 'In Memory', path: `/${IN_MEMORY}` },
+	// { label: 'Spark - Local', path: `/${SPARK_LOCAL}` },
+	// { label: 'Spark - Static', path: `/${SPARK_STATIC}` },
+	// { label: 'Spark - Kube', path: `/${SPARK_KUBE}` },
+	// { label: 'Build Parquet', path: '/build-parquet', alignToRight: true },
+	// { label: 'In Js', path: '/in-js' , alignToRight: true },
+	{ label: 'V2 - In Memory', path: `/${V2_MEMORY}` /*, alignToRight: true*/ },
 	{
 		label: 'V2 - Spark - Kube',
 		path: `/${V2_CLUSTER_KUBERNETES}`,
-		alignToRight: true,
+		/*alignToRight: true,*/
 	},
 ];
 
@@ -42,7 +35,7 @@ const Menu = () => {
 
 	return (
 		<div className="menu-container">
-			<WilcoMenu paths={realPaths} />
+			<WilcoMenu paths={realPaths} home={{ label: 'Home', path: V2_MEMORY }} />
 		</div>
 	);
 };

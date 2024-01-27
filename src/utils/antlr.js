9039d37726700c401f90171696e18a138a809df8
Nicolas Laval <nicolas.laval@making-sense.info>
2024-01-27 11:34:58 +0100

    Fix antlr utils

--- src/utils/antlr.js
+++ src/utils/antlr.js
@@ -1,5 +1,5 @@
 import { InputStream, CommonTokenStream } from 'antlr4';
-import { VtlLexer, VtlParser, VtlVisitor } from 'vtl-2-0-antlr-tools-ts';
+import { VtlLexer, VtlParser, VtlVisitor } from '@inseefr/vtl-2.0-antlr-tools';
 
 class AnalyzerVisitor extends VtlVisitor {
     constructor(persistent) {
@@ -13,13 +13,13 @@ class AnalyzerVisitor extends VtlVisitor {
 }
 
 export const getPersistentDatasets = (script) => {
-    if (!script) return [];
-    const chars = new InputStream(script);
-    const lexer = new VtlLexer(chars);
-    const tokens = new CommonTokenStream(lexer);
-    const parser = new VtlParser(tokens);
-    const pd = new Set();
-    const visitor = new AnalyzerVisitor(pd);
-    visitor.visit(parser.start());
-    return Array.from(pd);
+        if (!script) return [];
+        const chars = new InputStream(script);
+        const lexer = new VtlLexer(chars);
+        const tokens = new CommonTokenStream(lexer);
+        const parser = new VtlParser(tokens);
+        const pd = new Set();
+        const visitor = new AnalyzerVisitor(pd);
+        visitor.visit(parser.start());
+        return Array.from(pd);
 };

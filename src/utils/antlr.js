fde05775fa89bdb3eb551fc762ef223a08a7aa55
Nicolas Laval <nicolas.laval@making-sense.info>
2024-01-27 10:55:56 +0100

    Add getPersistentDatasets utilitary

---
+++ src/utils/antlr.js
@@ -0,0 +1,25 @@
+import { InputStream, CommonTokenStream } from 'antlr4';
+import { VtlLexer, VtlParser, VtlVisitor } from 'vtl-2-0-antlr-tools-ts';
+
+class AnalyzerVisitor extends VtlVisitor {
+    constructor(persistent) {
+        super();
+        this.persistent = persistent;
+    }
+
+    visitPersistAssignment = (ctx) => {
+        this.persistent.add(ctx.varID().getText());
+    };
+}
+
+export const getPersistentDatasets = (script) => {
+    if (!script) return [];
+    const chars = new InputStream(script);
+    const lexer = new VtlLexer(chars);
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new VtlParser(tokens);
+    const pd = new Set();
+    const visitor = new AnalyzerVisitor(pd);
+    visitor.visit(parser.start());
+    return Array.from(pd);
+};
